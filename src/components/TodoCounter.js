f0d3cda82d480701b62c33f76d14dcf9e84a178c
Lokitosi <andres.gamba2011@gmail.com>
2022-10-20 22:23:49 -0500

    First commit

---
+++ src/components/TodoCounter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/TodoCounter.css';
+
+
+function TodoCounter(){
+    return(
+        <h2 className="TodoCounter">Has Completado 2 de 3 Tareas</h2>
+    );
+};
+
+export {TodoCounter};
---
+++ src/components/TodoItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../styles/TodoItem.css";
+
+function TodoItem(props) {
+  return (
+    <li className="TodoItem">
+      <span
+        className={`Icon Icon-Check ${props.completed && "Icon-check--active"}`}
+      >
+        ✓
+      </span>
+      <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
+        {props.text}
+      </p>
+      <span
+        className="Icon Icon-delete"
+      >
+        X
+      </span>
+    </li>
+  );
+}
+
+export { TodoItem };
